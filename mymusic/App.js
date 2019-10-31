075b308f425d3636b69983d57daf15ed2f73c413
RobertVel666 <5k@iMac-de-Hector.local>
2019-10-31 08:39:42 -0600

    react native flux

--- mymusic/App.js
+++ mymusic/App.js
@@ -1,25 +1,64 @@
-import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
 
-import t from 'tcomb-form-native'; // 0.6.9
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, Button, Switch, TextInput, Alert} from 'react-native';
 
-const Form = t.form.Form;
-
-const User = t.struct({
-  email: t.String,
-  username: t.String,
-  password: t.String,
-  terms: t.Boolean
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
 });
 
-export default class App extends Component {
-  handleSubmit = () => {
-    // do the things  
+type Props = {};
+export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      textValue: "",
+    };
+  }
+  onChangeText =() =>{
+    const {text} = this.state
+    Alert.alert(text)
+  }
+
+  onChange = (value) => {
+    console.warn(`El switch cambiara a: ${value}`)
+    this.setState({switchValue: value})
+  }
+  onPressLearnMore(){
+    console.warn('Presionaste el boton')
   }
   render() {
     return (
-      <View style={styles.container}>
-        <Form type={User} /> {/* Notice the addition of the Form component */}
+      <View style = {styles.container}>
+      <Switch 
+        onValueChange = {() => this.onChange(!this.state.switchValue)}
+        value = {this.state.switchValue}
+      />
+      <Button
+          onPress={this.onPressLearnMore}
+          onPress={this.onChangeText}
+          title = "Learn More"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+        <Text style = {styles.welcome}> My music - Roberto</Text>
+        <Text style = {styles.instructions}>To get started, edit App.js</Text>
+        <Text style = {styles.instructions}>{instructions}</Text>
+        <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+        onChangeText={text => this.setState({text})}
+        text = {this.state.textValue}
+        placeholder = "Yeah"
+      />
       </View>
     );
   }
@@ -27,9 +66,19 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     justifyContent: 'center',
-    marginTop: 50,
-    padding: 20,
-    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
   },
 });
