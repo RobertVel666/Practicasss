195dcc6ea0ffdabd0e26aaa275f8537fb138abca
RobertVel666 <5k@iMac-de-Hector.local>
2019-11-14 08:39:48 -0600

    Flatlist

--- react/MiMusica/src/ArtistList.js
+++ react/MiMusica/src/ArtistList.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import { 
-    ListView,
     TouchableOpacity,
+    FlatList,
+    RefreshControl,
+    View
  } from 'react-native'
 
 import ArtistBox from './ArtistBox'
@@ -9,32 +11,26 @@ import ArtistBox from './ArtistBox'
 import { Actions } from 'react-native-router-flux'
 
 export default class ArtistList extends Component<Props> {
-    constructor(props) {
+    /*constructor(props) {
         super()
         const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
         this.state = {
             dataSource: ds
         }
+    }*/
+
+    renderRefreshControl() {
+        this.setState({ isLoading: true })
+        this.props.getArtist()
     }
-    componentDidMount(){
-        this.updateDataSource(this.props.artists)
-    }
- 
-    componentWillReceiveProps(newProps) {
-        if (newProps.artists !== this.props.artists) {
-            this.updateDataSource(newProps.artists)
-        }
-    }
-    updateDataSource = (data) => {
-        this.setState({
-            dataSource: this.state.dataSource.cloneWithRows(data)
-        })
-    }
+
     handlePress(artist) {
         Actions.artistDetail({artist: artist})
     }
+
     render(){
         return(
+            /*
             <ListView
                 enableEmptySections = {true}
                 dataSource = {this.state.dataSource}
@@ -45,6 +41,10 @@ export default class ArtistList extends Component<Props> {
                         </TouchableOpacity>
                     )
                 }}
+            />*/
+            <FlatList
+                data = {this.props.artist}
+                renderItem = {(artist)}
             />
         )
     }
